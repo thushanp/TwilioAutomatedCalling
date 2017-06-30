77356f4cb26dd2c25ba1a8493b8acae6e480116d
thushanp <tpuhalendran@college.harvard.edu>
2017-06-30 15:47:28 -0400

    audiotest3

--- sendcall.py
+++ sendcall.py
@@ -8,6 +8,7 @@ auth_token  = "5e7b2c6563764d0d62dc00e18e1b5853"
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(to="+16175159619", 
-    					   from_="+14054001401")
+    					   from_="+14054001401",
+    					   url="http://thushanp.github.io/parth")
 
-response.play('https://api.twilio.com/cowbell.mp3')
+print(call.sid)
